3b46baa31c511bd42f882972d504d1e0e73e89cf
KamranRasool <kamranrasool88@gmail.com>
2021-09-16 12:54:37 +0500

    Retrofit Added

---
+++ app/src/main/java/com/example/myapplication/EndPoints.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface EndPoints {
+
+    @GET("/posts")
+    Call<List<RetroPosts>> getAllPosts();
+    @GET("posts/{id}/comments")
+    Call<RetroComments> getComments(@Path(value = "id", encoded = true) String Id);
+}
---
+++ app/src/main/java/com/example/myapplication/RetroPosts.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class RetroPosts {
+
+    @SerializedName("userId")
+    private Integer userId;
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("body")
+    private String body;
+
+
+    public RetroPosts(Integer userId, Integer id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getbody() {
+        return body;
+    }
+
+    public void setbody(String body) {
+        this.body = body;
+    }
+
+}
